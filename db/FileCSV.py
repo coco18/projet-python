b59e71cab0132438ef8913a1d491764f79dc3c4c
Corentin MAY <corentin.may@etu.univ-nantes.fr>
2017-06-09 08:32:41 +0200

    correction bug indentention

--- db/FileCSV.py
+++ db/FileCSV.py
@@ -1,35 +1,35 @@
 import csv
 
 class FileCSV:
-  	"""Documentation for File CSV"""
-	def __init__(self,name):
-	"""Initialization of fileCSV
-	:param name: name of the file we want to convert
-	:type name: String"""
-		self.name = name
+    """Documentation for File CSV"""
+    def __init__(self,name):
+        """Initialization of fileCSV
+        :param name: name of the file we want to convert
+        :type name: String"""
+        self.name = name
 
 	#return tab of CSV file
-	def print_file(self):
-		tab =[]
-		with open(self.name) as file:
-			reader = csv.reader(file)
-			for row in reader:
-				tab.append(row)
-		return tab
+    def print_file(self):
+        tab =[]
+        with open(self.name) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                tab.append(row)
+        return tab
 
 	#print a specific line
-	def print_line(self,line):
-		tab = self.print_file()
-		return tab[column]
+    def print_line(self,line):
+        tab = self.print_file()
+        return tab[column]
 
 	#print line to line
-	def print_multiple_line(self,l1,l2):
-		tab = self.print_file()
-		return tab[l1:l2+1]
+    def print_multiple_line(self,l1,l2):
+        tab = self.print_file()
+        return tab[l1:l2+1]
 
-	def print_line_except_first(self):
-		tab = self.print_file()
-		return tab[1:len(tab)]
+    def print_line_except_first(self):
+        tab = self.print_file()
+        return tab[1:len(tab)]
 
 """
 f=FileCSV('fichiersCSV/23440003400026_J335_installations_table.csv')
--- model/equipmentactivity.py
+++ model/equipmentactivity.py
@@ -8,13 +8,13 @@ class EquipmentActivity():
     def create_object(self, tab):
         """Creation of the EquipmentActivity
     	:param tab: Array with the attribute of a equipment
-    	:type tab: array 
+    	:type tab: array
     	"""
         self.id_equipment = tab[2]
         self.id_activity = tab[4]
 
     def insert_in_DB(self, DB):
-         """insert the place in a table
+        """insert the place in a table
     	:param db: the database who contains the table
     	:type db: base
     	"""
@@ -23,11 +23,11 @@ class EquipmentActivity():
         c.execute(insert_query, (self.id_equipment, self.id_activity))
 
     def exist_in_DB(self, DB):
-    	"""Verify if the equipmentActivity exist in our table
+        """Verify if the equipmentActivity exist in our table
     	:param db: the base who contains the table
     	:type db: base
     	:rtype: boolean
-    	"""    
+    	"""
         c = DB.con.cursor()
         insert_query = "SELECT id_activity FROM equipmentactivity WHERE id_equipment=(?)"
         c.execute(insert_query, (self.id_equipment,))
